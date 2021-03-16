0209114d15a41d0bda0a94fb5429a4d4dde053e9
Marco Orazio Re <marcooraziore@gmail.com>
2021-03-16 12:01:04 +0100

    Refactoring
    - Trying to use more functions

--- js/script.js
+++ js/script.js
@@ -23,63 +23,136 @@
 // * con difficoltà 1 => tra 1 e 80
 // * con difficoltà 2 => tra 1 e 50
 
-function campoMinato() {
+function selectDifficulty(difficulty) {
+
    var difficulty = prompt("Seleziona un livello di difficoltà compresotra: 0, 1 o 2");
-   var maxNumbers = 0;
+   var lvlDiff    = {
 
+      levelDifficulty: difficulty,
+      maxNumbers: 0,
+   };
+   
    if (difficulty == 0) {
-
-      maxNumbers = 100;
+      
+      lvlDiff.maxNumbers = 100;
+      return lvlDiff;
    } else if (difficulty == 1) {
-
-      maxNumbers = 80;
+   
+      lvlDiff.maxNumbers = 80;
+      return lvlDiff;
    } else if (difficulty == 2) {
       
-      maxNumbers = 50;
+      lvlDiff.maxNumbers = 50;
+      return lvlDiff;
    } else {
       alert("Non hai selezionato la difficoltà");
    }
+}
 
-   console.log(maxNumbers);
+function pcRamdomNumbers(maxLng) {
+   
+   var pcNumbers = [];
 
-   var pcNumbers   = [];
-   var userNumbers = [];
-   var score       = 0;
-
-   while (pcNumbers.length < 16) {
-
-      var rndNumber = getRnd(1, maxNumbers);
+   while (pcNumbers.length < maxLng) {
+      
+      var rndNumber = getRnd(1, lvlDiff.maxNumbers);
 
       if (pcNumbers.includes(rndNumber)) {
          
       } else {
 
          pcNumbers.push(rndNumber);
-      }  
+      }
    }
 
-   console.log(pcNumbers);
+   return pcNumbers;
+}
 
-   while (userNumbers.length < (maxNumbers - 16)) {
+function userNumbers() {
+   
+   var userNumbers = [];
+   var score       = 0;
+   var userSummary = {
+      score: score,
+      userNumbers: userNumbers,
+      explosionNumber: 0
+   }
+
+   while (userNumbers.length < (lvlDiff.maxNumbers - pcNumbers.length)) {
       
       var userNumber = parseInt(prompt("Inserisci un numero (Non puoi inserire più volte uno stesso numero):"));
 
       if (userNumbers.includes(userNumber)) {
          
       } else if (pcNumbers.includes(userNumber)) {
-         
-         var explosionNumber = userNumber;
+
+         userSummary.explosionNumber = userNumber;
 
          break
       } else {
 
          userNumbers.push(userNumber);
-         score++
+         userSummary.score++;
       }
    }
 
-   console.log(userNumbers);
-   console.log("Partita terminata", "-", "Punteggio: " + score, "-", "Explosion number: " + explosionNumber);
-};
+   return userSummary;
+}
+
+
+var lvlDiff     = Object.assign({}, selectDifficulty());
+var pcNumbers   = pcRamdomNumbers(16);
+var userSummary = userNumbers();
+
+function campoMinato() {
+   
+   console.log("Partita terminata", "-", "Punteggio: " + userSummary.score, "-", "Explosion number: " + userSummary.explosionNumber);
+}
 
 campoMinato();
+
+// function campoMinato() {
+
+//    console.log(lvlDiff.maxNumbers);
+
+//    var pcNumbers   = [];
+//    var userNumbers = [];
+//    var score       = 0;
+
+//    while (pcNumbers.length < 16) {
+
+//       var rndNumber = getRnd(1, maxNumbers);
+
+//       if (pcNumbers.includes(rndNumber)) {
+         
+//       } else {
+
+//          pcNumbers.push(rndNumber);
+//       }  
+//    }
+
+//    console.log(pcNumbers);
+
+//    while (userNumbers.length < (maxNumbers - 16)) {
+      
+//       var userNumber = parseInt(prompt("Inserisci un numero (Non puoi inserire più volte uno stesso numero):"));
+
+//       if (userNumbers.includes(userNumber)) {
+         
+//       } else if (pcNumbers.includes(userNumber)) {
+         
+//          var explosionNumber = userNumber;
+
+//          break
+//       } else {
+
+//          userNumbers.push(userNumber);
+//          score++
+//       }
+//    }
+
+//    console.log(userNumbers);
+//    console.log("Partita terminata", "-", "Punteggio: " + score, "-", "Explosion number: " + explosionNumber);
+// };
+
+// campoMinato();
